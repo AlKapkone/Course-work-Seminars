9994a478fe8598fd101e925320a9aad6bda9721e
Orest Kapko <kapko2311@gmail.com>
2017-05-23 13:32:58 +0300

    Add files via upload

---
+++ Seminar5/src/main/java/seminar5/Starter.java
@@ -0,0 +1,11 @@
+package seminar5;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        
+        Client client = new Client();
+        client.start();
+        
+    }    
+}
